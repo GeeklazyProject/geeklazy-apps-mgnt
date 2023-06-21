e60029915281fe71d86ede5faf938404bee3bd2a
weiyang.li <weiyang.li@shuyun.com>
2023-06-21 16:50:01 +0800

    feat: openai应用初始化 --story=OPENAI_1 --user=geeklazy

--- apps/settings.py
+++ apps/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'openai.apps.OpenaiConfig',
+    'geeklazy_openai.apps.GeeklazyOpenaiConfig',
 ]
 
 MIDDLEWARE = [
---
+++ geeklazy_openai/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GeeklazyOpenaiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'geeklazy_openai'
